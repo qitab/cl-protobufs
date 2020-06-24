0f01c8c5b46a5eaca6e29f6c80914b3245e606ff
Ben Kuehnert <benkuehnert@google.com>
2020-06-24 20:43:57 +0000

    fix bug for non-repeated fields too

--- serialize.lisp
+++ serialize.lisp
@@ -700,7 +700,7 @@ See field-descriptor for the distinction between index, offset, and bool-number.
                          `(let ((,vval ,reader))
                             (when ,vval
                               (iincf ,size (encode-uint32 ,tag1 ,vbuf))
-                              ,(call-pseudo-method :serialize msg vval vbuf)
+                              (iincf ,size ,(call-pseudo-method :serialize msg vval vbuf))
                               (iincf ,size (encode-uint32 ,tag2 ,vbuf)))))
                        (let ((tag (make-tag $wire-type-string index)))
                          `(let ((,vval ,reader))
