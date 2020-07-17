cd8657399535b148a03088d9f45fe93816ee9a2d
Carl Gay <cgay@google.com>
2020-07-17 12:07:27 -0700

    shorten pointlessly long names in service-test
    This is just a precursor to working on the service code for real.
    
    PiperOrigin-RevId: 321827279

--- process-imports.lisp
+++ process-imports.lisp
@@ -10,10 +10,22 @@
 
 (in-package "PROTO-IMPL")
 
+;; This is an artifact of the cl-protobufs implementation
+;; and the case that someone is using define-schema instead of
+;; just writting a .proto file.
+;; The function here is defined in cl-protobufs.asdf-support.lisp
+;; and is really ASDF specific and not usefull in blaze.
+;; So, we define it here like this.
+
 (defun proto-impl::process-imports (schema imports)
-  "Validated all of the files given by 'imports' are imported.
+  "Imports all of the files given by 'imports'.
    If the file is a .proto file, it first parses it and writes a .lisp file.
    The .lisp file is than compiled and loaded."
+  ;; cl-protobufs have only functionality implemented for ASDF.lisp
+  ;; which assumes that all .proto dependencies are present in the file system
+  ;; and accessible by full truename. Since we do not have the ASDF assumptions valid,
+  ;; the implementation uses the pathname to find the 'schemata' assuming that
+  ;; .proto.fasl libraries registered themselves as per the mechanism above.
   (dolist (import (reverse imports))
     (let* ((imported (proto:find-schema (if (stringp import) (pathname import) import))))
       (unless imported
