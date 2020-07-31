6913383ecfcab4f377c07b20581603aca1f67232
jgodbout <jgodbout@google.com>
2020-07-31 10:21:17 -0700

    Internal change
    PiperOrigin-RevId: 324233246

---
+++ new.lisp
@@ -0,0 +1,9 @@
+;;; ;;; Copyright 2012-2020 Google LLC
+;;;
+;;; Use of this source code is governed by an MIT-style
+;;; license that can be found in the LICENSE file or at
+;;; https://opensource.org/licenses/MIT.
+
+(in-package "PROTO-IMPL")
+
+(print 'here)
