f308cf82860d82fa81debdb067d8cc3ab7776eba
Carl Gay <carlgay@gmail.com>
2020-08-02 13:53:12 +0000

    Replace unused packages with cl-protobufs.test

--- tests/pkgdcl.lisp
+++ tests/pkgdcl.lisp
@@ -6,50 +6,7 @@
 
 (in-package "CL-USER")
 
-
-;;; Package declaration for Protobufs tests
-
-(defpackage protobufs-test
-  (:nicknames :proto-test)
-  (:use :common-lisp :protobufs :protobufs-implementation)
-  (:shadowing-import-from :protobufs-implementation
-   "FIND-METHOD")
-  #+test-tools
-  (:import-from :qtest
-   "DEFINE-TEST"
-   "DEFINE-TEST-SUITE"
-   "REGISTER-TEST"
-   "RUN-TEST"
-   "ASSERT-EQUAL"
-   "ASSERT-TRUE"
-   "ASSERT-FALSE"
-   "ASSERT-ERROR")
-  (:export
-   "DEFINE-TEST"
-   "DEFINE-TEST-SUITE"
-   "REGISTER-TEST"
-   "RUN-TEST"
-   #-test-tools
-   "RUN-ALL-TESTS"
-   "ASSERT-EQUAL"
-   "ASSERT-TRUE"
-   "ASSERT-FALSE"
-   "ASSERT-ERROR"))
-
-
-;;; Packages used by .proto files
-
-(defpackage protobuf-unittest
-  (:use :common-lisp :protobufs)
-  (:nicknames :pbtest))
-
-(defpackage protobuf-unittest-import
-  (:use :common-lisp :protobufs)
-  (:nicknames :pbtestimp))
-
-(defpackage protobuf-geodata
-  (:use :common-lisp :protobufs)
-  (:nicknames :geodata))
-
-(defpackage protobuf-forward-reference-unittest
-  (:use :common-lisp :protobufs))
+(defpackage #:cl-protobufs.test
+  (:use #:cl)
+  (:export #:root-suite
+           #:run-all))
